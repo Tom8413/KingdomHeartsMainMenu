feb58e20ebf973d27d51be3056e36a1f9de3bfd6
Tomasz <tomaszszubarczyk@gmail.com>
2019-06-01 15:57:45 +0200

    Second Commit
    Add pages choiceLV, vibration, proceed

--- Kingdom Hearts Menu/Assets/Scripts/ButtonController.cs
+++ Kingdom Hearts Menu/Assets/Scripts/ButtonController.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour
 {
     public AudioSource music;
     public AudioClip sound1;
     public AudioClip sound2;
-   
-   
 
     public void MoveSound()
     {
